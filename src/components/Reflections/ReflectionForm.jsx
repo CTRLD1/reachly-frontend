1074ad8fa7cd7935569549be32afbc4015fa18b5
dansubaie2 <dansubaie029@gmail.com>
2025-11-05 13:33:14 +0300

    added styling to the Reflections Add/Edit Form

--- src/components/Reflections/ReflectionForm.jsx
+++ src/components/Reflections/ReflectionForm.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router'
 import axios from 'axios'
 import { authRequest, getUserFromToken, clearTokens } from "../../lib/auth"
+import Forms from '../ui/Forms'
 
 // ref: cat-collector classwork code
 
@@ -20,14 +21,14 @@ function ReflectionForm() {
     const [userChallenges, setUserChallenges] = useState([])
 
     async function getSingleReflection() {
-        const response = await authRequest({method:'get', url:`${URL}/reflections/${reflectionId}/`})
+        const response = await authRequest({ method: 'get', url: `${URL}/reflections/${reflectionId}/` })
         console.log(response.data)
         setFormData(response.data)
     }
 
     async function getUserChallenges() {
         try {
-            const response = await authRequest({method:'get', url:`${URL}/userchallenges/`})
+            const response = await authRequest({ method: 'get', url: `${URL}/userchallenges/` })
             setUserChallenges(response.data)
         } catch (err) {
             console.error('Error fetching challenges')
@@ -52,9 +53,9 @@ function ReflectionForm() {
         let response = {}
         try {
             if (reflectionId) {
-                response = await authRequest({method:'patch', url:`${URL}/reflections/${reflectionId}/`, data:formData})
+                response = await authRequest({ method: 'patch', url: `${URL}/reflections/${reflectionId}/`, data: formData })
             } else {
-                response = await authRequest({method:'post', url:`${URL}/reflections/`, data:formData})
+                response = await authRequest({ method: 'post', url: `${URL}/reflections/`, data: formData })
             }
             console.log(response)
             if (response.status === 201 || response.status === 200) {
@@ -72,24 +73,11 @@ function ReflectionForm() {
     return (
         <div>
             <h1>{reflectionId ? `Edit Reflection` : 'Add a New Reflection'} </h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='text'>Your Reflection</label>
-                    <textarea id='text' name='text' value={formData.text} onChange={handleChange} required />
-                </div>
+            <Forms onSubmit={handleSubmit}>
 
-                <div>
-                    <label htmlFor='mood'>Mood</label>
-                    <select id='mood' name='mood' value={formData.mood} onChange={handleChange} >
-                        <option value='H'>Happy😄</option>
-                        <option value='N'>Neutral😐</option>
-                        <option value='S'>Sad😔</option>
-                    </select>
-                </div>
-
-                <div>
-                    <label htmlFor='user_challenge'>Select a Challenge</label>
-                    <select id='user_challenge' name='user_challenge' value={formData.user_challenge} onChange={handleChange} required>
+                <div className='flex flex-col space-y-1'>
+                    <label htmlFor='user_challenge' className='text-gray-300 text-sm'>Select a Challenge</label>
+                    <select id='user_challenge' name='user_challenge' value={formData.user_challenge} onChange={handleChange} required className='w-full p-3 rounded-xl bg-transparent text-white border-2 border-gray-600 focus:border-blue-500 outline-none'>
                         <option value=''>choose a challenge</option>
                         {userChallenges.map(userchallenge => (
                             <option key={userchallenge.id} value={userchallenge.id}>
@@ -100,10 +88,25 @@ function ReflectionForm() {
                         }
                     </select>
                 </div>
-                <button type='submit'>submit</button>
+
+                <div className='flex flex-col space-y-1 mt-4'>
+                    <label htmlFor='mood' className='text-gray-300 text-sm'>Mood</label>
+                    <select id='mood' name='mood' value={formData.mood} onChange={handleChange} className='w-full p-3 rounded-xl bg-transparent text-white border-2 border-gray-600 focus:border-blue-500 outline-none' >
+                        <option value='H'>Happy😄</option>
+                        <option value='N'>Neutral😐</option>
+                        <option value='S'>Sad😔</option>
+                    </select>
+                </div>
+
+                <div className='flex flex-col space-y-1 mt-4'>
+                    <label htmlFor='text' className='text-gray-300 text-sm'>Your Reflection</label>
+                    <textarea id='text' name='text' value={formData.text} onChange={handleChange} required className='w-full p-3 rounded-xl bg-transparent text-white border-2 border-gray-600 focus:border-blue-500 outline-none min-h-[120px]' />
+                </div>
+
+                <button type='submit' className='w-full bg-blue-600 hover:bg-blue-700 py-2 rounded-2xl text-white font-semibold transition duration-200 mt-3'>submit</button>
 
 
-            </form>
+            </Forms>
         </div>
     )
 }
